087ad73c389d2689b2331b7d48ff547ae81a5222
akashpadwal <akashpadwal>
2021-06-09 20:15:38 +0530

    1

--- src/main/App.java
+++ src/main/App.java
@@ -1,13 +1,11 @@
-package com.mycompany.app;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+package com.mycompany;
+public class App  
+{   
+public static void main(String[] args)    
+{           
+System.out.print("Enter a string: ");   
+//reading a string from the user  
+String name = System.console().readLine();   
+System.out.println("You have entered: "+name);   
+}   
+}  
